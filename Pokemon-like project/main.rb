eec3c635fe8f0a6acfe9a40307326db34b9e54d6
jiangbw210 <57691075+jiangbw210@users.noreply.github.com>
2021-02-17 17:11:18 -0600

    Update main.rb

--- Pokemon-like project/main.rb
+++ Pokemon-like project/main.rb
@@ -23,69 +23,158 @@ def tick args
   item2box = [40, 80, 100, 40, 0, 0, 170]
   item3box = [40, 30, 100, 40, 0, 0, 170]
   
-#This part shows where is the box that player can click, you can put # in front of them to hide the blue box in game
+case args.state.button
+  when 0
   args.outputs.borders << attackbox
+  when 1
   args.outputs.borders << defendbox
+  when 2
   args.outputs.borders << itembox
+  when 3
   args.outputs.borders << item1box
+  when 4
   args.outputs.borders << item2box
+  when 5
   args.outputs.borders << item3box
-#This part shows where is the box that player can click, you can put # in front of them to hide the blue box in game
-
+end
+ case args.state.select
+  when 0
+  
+  when 1
+  args.outputs.borders << [490, 360, 280, 40]
+  args.outputs.labels << mylabel(args, 500, 16, "You attacked the enemy")
+  
+  when 2
+  args.outputs.borders << [490, 360, 220, 40]
+  args.outputs.labels << mylabel(args, 500, 16, "You choose defend")
+  when 3
+   args.outputs.borders << [490, 360, 380, 40]	      
+   args.outputs.labels << mylabel(args, 500, 16, "Choose the item you want to use")
+   args.outputs.labels << [50, 160, "ITEM 1", 4]
+   args.outputs.labels << [50, 110, "ITEM 2", 4]
+   args.outputs.labels << [50, 60,  "ITEM 3", 4]
+end
+  pos = args.inputs.mouse.position
   x = 500
+  args.state.select ||= 0
+  args.state.wait||=0
   args.state.selectitem||=0
+  args.state.button||=0 #set it defualt 0 which is attack, by change this, change the position of cursor
+  # 0 attack | 1 defend | 2 item | 3 item1 | 4 item2 | 5 item3
+  
   #animate cat
   looping_animation_cat args
   looping_animation_jellyfish args
+  #keyboard
+#  args.state.enter_pressed_at ||= false
+# if args.inputs.keyboard.key_down.up and args.state.button > 0
+#    args.state.button -=1
+#  end
   
+#  if args.inputs.keyboard.key_down.up and args.state.button < 2
+#    args.state.button +=1
+#  end
+  
+  
+#  if args.inputs.keyboard.key_up.enter
+#    args.state.enter_pressed_at  = args.state.tick_count
+#	args.state.select = 0
+#  end
+#  if args.state.enter_pressed_at
+    
+	#attack
+#	if args.state.button == 0
+#	  args.state.wait += 1
+#	  if args.state.wait == 20	
+#	  args.state.select = 1       
+#	  args.state.enter_pressed_at = false
+#	  args.state.wait = 0
+#	  end
+#	end
+	#defend
+#	if args.state.button == 1
+#	  args.state.select = 2
+#	  args.state.wait += 1
+#     if args.state.wait == 20	  
+#	  args.state.enter_pressed_at = false
+#	  args.state.wait = 0
+#	  end
+#	end
+#  end
+  #mouse part
+#  args.outputs.labels << [pos.x + 10, pos.y + 10, "#{pos}"] # outputs label of coordinates
+#  args.outputs.solids << [pos.x -  2, pos.y - 2, 5, 5] # outputs small blackk box placed where mouse is
+
   if args.inputs.mouse.click
     args.state.last_mouse_click = args.inputs.mouse.click
   end
-  
+  if pos.inside_rect? attackbox
+	args.state.button = 0
+  end
+  if pos.inside_rect? defendbox
+	args.state.button = 1
+  end
+  if pos.inside_rect? itembox
+	args.state.button = 2
+  end
+  if pos.inside_rect? item1box and args.state.selectitem != 0
+	args.state.button = 3
+  end
+   if pos.inside_rect? item2box and args.state.selectitem != 0
+	args.state.button = 4
+  end
+   if pos.inside_rect? item3box and args.state.selectitem != 0
+	args.state.button = 5
+  end
   #attack
   if args.state.last_mouse_click 
     if args.state.last_mouse_click.point.inside_rect? attackbox
-	  args.outputs.borders << [490, 360, 280, 40]
-      args.outputs.labels << mylabel(args, x, 16, "You attacked the enemy")
-	 	
-		
-
+	  args.state.select = 1
     end
 	#defend
-	 if args.state.last_mouse_click.point.inside_rect? defendbox 
-	  args.state.select+=1
-	 	  args.outputs.borders << [490, 360, 220, 40]
-      args.outputs.labels << mylabel(args, x, 16, "You choose defend")
-		
-		 
-	    end 
+	if args.state.last_mouse_click.point.inside_rect? defendbox 
+	  args.state.select = 2
+	end 
 		
 	#item
-	  if args.state.last_mouse_click.point.inside_rect? itembox 
-	  	  args.outputs.borders << [490, 360, 380, 40]	      
-          args.outputs.labels << mylabel(args, x, 16, "Choose the item you want to use")
-		   args.outputs.labels << [50, 160, "ITEM 1", 4]
-           args.outputs.labels << [50, 110, "ITEM 2", 4]
-           args.outputs.labels << [50, 60,  "ITEM 3", 4]
-		   args.state.selectitem+=1
-	      
-	   end
-	  if args.state.last_mouse_click.point.inside_rect? item1box and args.state.selectitem != 0
+	if args.state.last_mouse_click.point.inside_rect? itembox 
+		   args.state.select = 3
+		   args.state.selectitem = 1
+
+	end
+	  
+	  if args.state.last_mouse_click.point.inside_rect? item1box and args.state.selectitem == 1
 		 args.outputs.borders << [490, 360, 200, 40]
 		 args.outputs.labels << mylabel(args, x, 16, "You use item 1")
-		 
-		 args.state.selectitem-=1
+		 args.state.wait+=1
+		 args.state.select = 0
+		 if args.state.wait == 20
+		 args.state.selectitem = 0
+		 args.state.button = 0
+		 args.state.wait = 0
+		 end
 		end
-	   if args.state.last_mouse_click.point.inside_rect? item2box and args.state.selectitem != 0
+	   if args.state.last_mouse_click.point.inside_rect? item2box and args.state.selectitem == 1
 		 args.outputs.borders << [490, 360, 200, 40]
 		 args.outputs.labels << mylabel(args, x, 16, "You use item 2")
-		 args.state.selectitem-=1
+		 args.state.wait+=1
+		 args.state.select = 0
+		 if args.state.wait == 20
+		 args.state.selectitem = 0
+		 args.state.button = 0
+		 args.state.wait = 0
+		 end
 		end
-	   if args.state.last_mouse_click.point.inside_rect? item3box and args.state.selectitem != 0
+	   if args.state.last_mouse_click.point.inside_rect? item3box and args.state.selectitem == 1
 		 args.outputs.borders << [490, 360, 200, 40]
-		 args.outputs.labels << mylabel(args, x, 16, "You use item 3")
-		 
-		 args.state.selectitem-=1
+		 args.outputs.labels << mylabel(args, x, 16, "You use item 3")		 
+		 args.state.wait+=1
+		 args.state.select = 0
+		 if args.state.wait == 20
+		 args.state.selectitem = 0
+		 args.state.button = 0
+		 args.state.wait = 0
+		 end
 		end
   end
 end
@@ -160,3 +249,6 @@ def looping_animation_cat args
   # Now that we have `sprite_index, we can present the correct file.
   args.outputs.sprites << [200, 200, 200, 200, "sprites/cat_sprite/Cat_#{sprite_index}.png"]
 end
+
+
+
