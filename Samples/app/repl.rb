d95523bc6fb2c86c0fa5f2c1f9dafec1f3f471b0
Pyrosix <nlacewell97@gmail.com>
2021-03-23 16:01:05 -0500

    typewriter text
    text appears one at a time

--- Samples/app/repl.rb
+++ Samples/app/repl.rb
@@ -1,32 +1,16 @@
-xrepl do
+repl do
 
     def s
         return $game.s
     end
-    delta = (s.player.x - s.camera[:x] - 100)
-    puts delta
-    s.camera[:x] += delta * 0.01
-    puts s.camera[:x]
-    puts "------------"
-    has_platforms = s.platforms.find do |p| 
-        puts "platforms"
-        puts p
-        puts "delta"
-        puts delta
-        puts "camera"
-        puts s.camera[:x]
-        p.x > (s.player.x + 300) 
-    end
-    puts has_platforms
-  end
 
-  repl do 
-   $game.move_spawn
-    def s
-        return $game.s
-    end
-    puts "platform."
-    has_platforms = s.platforms.find_all { |p| p.x > (s.player.x + 300) }
-    puts has_platforms
+    def schedule_string start_at, string
 
+        result = []
+        string.each_char.with_index do |s, i|
+            result << {s: s, i: start_at + i * 60} 
+        end
+        return result
+    end
+        puts schedule_string 60, "Scrolling text string"
   end
